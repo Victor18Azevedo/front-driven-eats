6c52af2861cf3060b525cc3e42e218e957bd8fac
Victor Azevedo <josevcazevedo@gmail.com>
2022-08-13 10:52:11 -0300

    v1-atualizado cardapio, correcao bug

--- scripts/script.js
+++ scripts/script.js
@@ -1,103 +1,96 @@
-const telefonePedido = 5583981129704;
+const telefonePedido = 5585997492998;
+let pratoNome;
+let bebidaNome;
+let sobremesaNome;
+let pratoPreco;
+let bebidaPreco;
+let sobremesaPreco;
+let precoTotal;
 
 function selecionarItem(itemSelecionado) {
-  const regexPrato = /prato/;
-  const regexBebida = /bebida/;
-  const regexSobremesa = /sobremesa/;
-
   const pratoAnterior = document.querySelector(".pratos .selecionado");
   const bebidaAnterior = document.querySelector(".bebidas .selecionado");
   const sobremesaAnterior = document.querySelector(".sobremesas .selecionado");
 
-  //Se item for prato
-  if (regexPrato.test(itemSelecionado)) {
-    if (pratoAnterior !== null) {
+  //Verifica se selecao eh prato
+  if (itemSelecionado.classList.contains("prato")) {
+    //Remove selecao anterior
+    if (pratoAnterior) {
       pratoAnterior.classList.remove("selecionado");
       const icon = pratoAnterior.querySelector(".icon-check");
       icon.classList.remove("icon-check");
     }
-    const pratoSelecionado = document.querySelector(itemSelecionado);
-    pratoSelecionado.classList.add("selecionado");
-    const icon = pratoSelecionado.querySelector("ion-icon");
+
+    //Realiza selecao atual
+    itemSelecionado.classList.add("selecionado");
+    const icon = itemSelecionado.querySelector("ion-icon");
     icon.classList.add("icon-check");
+
+    //Atualza valores Nome e Preco
+    pratoNome = itemSelecionado.querySelector(".item-nome").innerHTML;
+    pratoPreco = Number(
+      itemSelecionado.querySelector(".item-preco").innerHTML.replace(",", ".")
+    );
   }
 
-  //Se item for bebida
-  if (regexBebida.test(itemSelecionado)) {
-    if (bebidaAnterior !== null) {
+  //Verifica se selecao eh bebida
+  if (itemSelecionado.classList.contains("bebida")) {
+    //Remove selecao anterior
+    if (bebidaAnterior) {
       bebidaAnterior.classList.remove("selecionado");
       const icon = bebidaAnterior.querySelector(".icon-check");
       icon.classList.remove("icon-check");
     }
-    const bebidaSelecionado = document.querySelector(itemSelecionado);
-    bebidaSelecionado.classList.add("selecionado");
-    const icon = bebidaSelecionado.querySelector("ion-icon");
+
+    //Realiza selecao atual
+    itemSelecionado.classList.add("selecionado");
+    const icon = itemSelecionado.querySelector("ion-icon");
     icon.classList.add("icon-check");
+
+    //Atualza valores Nome e Preco
+    bebidaNome = itemSelecionado.querySelector(".item-nome").innerHTML;
+    bebidaPreco = Number(
+      itemSelecionado.querySelector(".item-preco").innerHTML.replace(",", ".")
+    );
   }
 
-  //Se item for sobremesa
-  if (regexSobremesa.test(itemSelecionado)) {
-    if (sobremesaAnterior !== null) {
+  //Verifica se selecao eh sobremesa
+  if (itemSelecionado.classList.contains("sobremesa")) {
+    //Remove selecao anterior
+    if (sobremesaAnterior) {
       sobremesaAnterior.classList.remove("selecionado");
       const icon = sobremesaAnterior.querySelector(".icon-check");
       icon.classList.remove("icon-check");
     }
-    const sobremesaSelecionado = document.querySelector(itemSelecionado);
-    sobremesaSelecionado.classList.add("selecionado");
-    const icon = sobremesaSelecionado.querySelector("ion-icon");
+
+    //Realiza selecao atual
+    itemSelecionado.classList.add("selecionado");
+    const icon = itemSelecionado.querySelector("ion-icon");
     icon.classList.add("icon-check");
+
+    //Atualza valores Nome e Preco
+    sobremesaNome = itemSelecionado.querySelector(".item-nome").innerHTML;
+    sobremesaPreco = Number(
+      itemSelecionado.querySelector(".item-preco").innerHTML.replace(",", ".")
+    );
   }
 
-  //Hablita botao apos a escolha dos 3 itens
+  precoTotal = pratoPreco + bebidaPreco + sobremesaPreco;
+  const pedidoMensagem = `Olá, gostaria de fazer o pedido:
+- Prato: ${pratoNome}
+- Bebida: ${bebidaNome}
+- Sobremesa: ${sobremesaNome}
+Total: R$ ${precoTotal.toFixed(2)}`;
+
+  //Habilita botao (Fazer Pedido) caso um item cada categorias esteja selecionado
   if (
-    document.querySelector(".pratos .selecionado") !== null &&
-    document.querySelector(".bebidas .selecionado") !== null &&
-    document.querySelector(".sobremesas .selecionado") !== null
+    document.querySelector(".pratos .selecionado") &&
+    document.querySelector(".bebidas .selecionado") &&
+    document.querySelector(".sobremesas .selecionado")
   ) {
-    const pedidoMensagem = mensagem();
-    1;
     const btn = document.querySelector(".btn-container");
     btn.innerHTML = `<a href='https://wa.me/${telefonePedido}?text=${encodeURIComponent(
       pedidoMensagem
     )}' target=_blank><button class="btn-pedir btn-submit">Fazer o pedido</button></a>`;
   }
-}
-
-function mensagem() {
-  //Nome dos itens selecionados
-  const pratoNome = document.querySelector(
-    ".pratos .selecionado .item-nome"
-  ).innerHTML;
-  const bebidaNome = document.querySelector(
-    ".bebidas .selecionado .item-nome"
-  ).innerHTML;
-  const sobremesaNome = document.querySelector(
-    ".sobremesas .selecionado .item-nome"
-  ).innerHTML;
-
-  //Preco Total:
-  const pratoPreco = Number(
-    document
-      .querySelector(".pratos .selecionado .valor")
-      .innerHTML.replace(",", ".")
-  );
-  const bebidaPreco = Number(
-    document
-      .querySelector(".bebidas .selecionado .valor")
-      .innerHTML.replace(",", ".")
-  );
-  const sobremesaPreco = Number(
-    document
-      .querySelector(".sobremesas .selecionado .valor")
-      .innerHTML.replace(",", ".")
-  );
-  const precoTotal = pratoPreco + bebidaPreco + sobremesaPreco;
-
-  const pedidoMensagem = `Olá, gostaria de fazer o pedido:
-- Prato: ${pratoNome}
-- Bebida: ${bebidaNome}
-- Sobremesa: ${sobremesaNome}
-Total: R$ ${precoTotal}`;
-
-  return pedidoMensagem;
 }
