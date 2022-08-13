1e9236c5559dec233ad7f3b2dd837358461e0c40
Victor Azevedo <josevcazevedo@gmail.com>
2022-08-13 16:54:47 -0300

    implementado bonus

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,9 @@ let bebidaPreco;
 let sobremesaPreco;
 let precoTotal;
 
+let clienteNome;
+let clienteEndereco;
+
 function selecionarItem(itemSelecionado) {
   const pratoAnterior = document.querySelector(".pratos .selecionado");
   const bebidaAnterior = document.querySelector(".bebidas .selecionado");
@@ -76,11 +79,6 @@ function selecionarItem(itemSelecionado) {
   }
 
   precoTotal = pratoPreco + bebidaPreco + sobremesaPreco;
-  const pedidoMensagem = `Olá, gostaria de fazer o pedido:
-- Prato: ${pratoNome}
-- Bebida: ${bebidaNome}
-- Sobremesa: ${sobremesaNome}
-Total: R$ ${precoTotal.toFixed(2)}`;
 
   //Habilita botao (Fazer Pedido) caso um item cada categorias esteja selecionado
   if (
@@ -88,9 +86,53 @@ Total: R$ ${precoTotal.toFixed(2)}`;
     document.querySelector(".bebidas .selecionado") &&
     document.querySelector(".sobremesas .selecionado")
   ) {
-    const btn = document.querySelector(".btn-container");
-    btn.innerHTML = `<a href='https://wa.me/${telefonePedido}?text=${encodeURIComponent(
-      pedidoMensagem
-    )}' target=_blank><button class="btn-pedir btn-submit">Fazer o pedido</button></a>`;
+    const btn = document.querySelector(".btn-pedir");
+    btn.classList.remove("btn-wait");
+    btn.classList.add("btn-submit");
+    btn.innerHTML = "Fazer o pedido";
+    // btn.innerHTML = `<a href='https://wa.me/${telefonePedido}?text=${encodeURIComponent(
+    //   pedidoMensagem
+    // )}' target=_blank><button class="btn-pedir btn-submit"></button></a>`;
   }
+}
+
+function fazerPedido() {
+  const aviso = document.querySelector(".aviso-confirma-pedido");
+  aviso.classList.remove("hide");
+
+  //Mostrar dados itens selecionados
+  document.querySelector(".prato-pedido-nome").innerHTML = pratoNome;
+  document.querySelector(".prato-pedido-preco").innerHTML =
+    pratoPreco.toFixed(2);
+  document.querySelector(".bebida-pedido-nome").innerHTML = bebidaNome;
+  document.querySelector(".bebida-pedido-preco").innerHTML =
+    bebidaPreco.toFixed(2);
+  document.querySelector(".sobremesa-pedido-nome").innerHTML = sobremesaNome;
+  document.querySelector(".sobremesa-pedido-preco").innerHTML =
+    sobremesaPreco.toFixed(2);
+  document.querySelector(".total-preco").innerHTML = precoTotal.toFixed(2);
+}
+
+function refazerPedido() {
+  const aviso = document.querySelector(".aviso-confirma-pedido");
+  aviso.classList.add("hide");
+}
+
+function confirmarPedido() {
+  //Ler nome e endereco
+  clienteNome = prompt("Seu nome:");
+  clienteEndereco = prompt("Seu endereço:");
+
+  const pedidoMensagem = `Olá, gostaria de fazer o pedido:
+- Prato: ${pratoNome}
+- Bebida: ${bebidaNome}
+- Sobremesa: ${sobremesaNome}
+Total: R$ ${precoTotal.toFixed(2)}
+  
+Nome: ${clienteNome}
+Endereço: ${clienteEndereco}`;
+
+  window.open(
+    `https://wa.me/${telefonePedido}?text=${encodeURIComponent(pedidoMensagem)}`
+  );
 }
